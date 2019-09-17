f26307c7e59628a2015ef061982e5dc1ab662a04
subhranshu <subhranshu.pujari@hotmail.com>
2019-09-17 21:16:59 +0530

    first commit

---
+++ src/main/java/com/oxygen/restservices/RestservicesSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.oxygen.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicesSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestservicesSimpleApplication.class, args);
+	}
+
+}
